c58052af762365286c2fe876184927fa1f8da85c
marianne.trizha <marianne.escubin@gmail.com>
2017-12-12 21:38:42 +0800

    javascript

---
+++ mod05-02/js/script.js
@@ -0,0 +1,27 @@
+function redFunc() {
+	document.getElementById("red-box").style.backgroundColor = "red";
+}
+
+function oraFunc() {
+	document.getElementById("orange-box").style.backgroundColor = "orange";
+}
+
+function orangeFunc() {
+	document.getElementById("yellow-box").style.backgroundColor = "yellow";
+}
+
+function greenFunc() {
+	document.getElementById("green-box").style.backgroundColor = "green";
+}
+
+function blueFunc() {
+	document.getElementById("blue-box").style.backgroundColor = "blue";
+}
+
+function indigoFunc() {
+	document.getElementById("indigo-box").style.backgroundColor = "indigo";
+}
+
+function redFunc() {
+	document.getElementById("red-box").style.backgroundColor = "red";
+}
