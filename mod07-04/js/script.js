de126231417d0e54850eef1785d671d6f96f08cb
marianne.trizha <marianne.escubin@gmail.com>
2017-12-14 20:36:52 +0800

    Switcher

---
+++ mod07-04/js/script.js
@@ -0,0 +1,50 @@
+function changeBackgroundColor() {
+	var newColor = document.getElementById("newBackgroundColor").value;
+	// console.log(newColor);
+
+	switch (newColor) {
+		case "red":
+			document.getElementById("mainWrapper").style.backgroundColor = "red";
+			document.getElementById("theMessage").innerHTML = "Your new background color is red!";
+		break;
+
+		case "orange":
+			document.getElementById("mainWrapper").style.backgroundColor = "orange";
+			document.getElementById("theMessage").innerHTML = "Your new background color is red!";
+		break;
+
+		case "yellow":
+			document.getElementById("mainWrapper").style.backgroundColor = "yellow";
+			document.getElementById("theMessage").innerHTML = "Your new background color is red!";
+		break;
+
+		case "red":
+			document.getElementById("mainWrapper").style.backgroundColor = "red";
+			document.getElementById("theMessage").innerHTML = "Your new background color is red!";
+		break;
+
+		case "red":
+			document.getElementById("mainWrapper").style.backgroundColor = "red";
+			document.getElementById("theMessage").innerHTML = "Your new background color is red!";
+		break;
+
+		case "red":
+			document.getElementById("mainWrapper").style.backgroundColor = "red";
+			document.getElementById("theMessage").innerHTML = "Your new background color is red!";
+		break;
+
+		case "red":
+			document.getElementById("mainWrapper").style.backgroundColor = "red";
+			document.getElementById("theMessage").innerHTML = "Your new background color is red!";
+		break;
+
+		case "red":
+			document.getElementById("mainWrapper").style.backgroundColor = "red";
+			document.getElementById("theMessage").innerHTML = "Your new background color is red!";
+		break;
+
+
+
+
+	}
+}
