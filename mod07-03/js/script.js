522e5b9bac3dfd1a303bf7e517f30712ff5a6046
marianne.trizha <marianne.escubin@gmail.com>
2017-12-14 20:11:21 +0800

    Odd Even Identifier with Button

---
+++ mod07-03/js/script.js
@@ -0,0 +1,16 @@
+function identifyThis() {
+	var num = parseInt(document.getElementById("numberInput").value);
+	// console.log(num);
+
+	if (num % 2 == 0) {
+		document.getElementById("theMessage").innerHTML = "It is an even number!";
+		document.getElementById("mainWrapper").style.backgroundColor = "red";
+	} 
+	else {
+		document.getElementById("theMessage").innerHTML = "It is an odd number!";
+		document.getElementById("mainWrapper").style.backgroundColor = "blue";
+}
+
+	document.getElementById("header1").style.color = "white";
+}
+
